b62eb418751138d03d7f1bf8f481d718910cfb2c
lanyulei <fdevops@163.com>
2025-06-12 19:00:29 +0800

    feat: 实现数据验证功能，查询模型字段并解析数据

--- pkg/server/data.go
+++ pkg/server/data.go
@@ -1,5 +1,38 @@
 package server
 
-func VerifyData() {
+import (
+	"encoding/json"
+	"fmt"
+	"openops/app/resource/models"
 
+	"github.com/lanyulei/toolkit/db"
+)
+
+func VerifyData(data *models.Data) (ok bool, err error) {
+	var (
+		fieldList []*models.Field
+		fieldMaps = make(map[string]*models.Field)
+		dataMaps  map[string]interface{}
+	)
+
+	// 1. 查询模型 ID 对应的字段列表
+	err = db.Orm().Model(&models.Field{}).Where("model_id = ?", data.ModelId).Find(&fieldList).Error
+	if err != nil {
+		return
+	}
+
+	for _, field := range fieldList {
+		fieldMaps[field.Key] = field
+	}
+
+	err = json.Unmarshal(data.Data, &dataMaps)
+	if err != nil {
+		return
+	}
+
+	for key, value := range dataMaps {
+		fmt.Println(key, value)
+	}
+
+	return
 }
